e3b0f9735099251ac5cfbe059ca47a0aa6cb0b77
Conrad Beach <conradbeach@gmail.com>
2017-02-01 09:02:38 -0700

    Create a few constants.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
+  ERROR_COLOR = "red darken-4".freeze
+  SUCCESS_COLOR = "green darken-1".freeze
+  BUTTON_CLASS = "btn waves-effect waves-light".freeze
+
   def get_flash_color(name)
     if name == "error"
-      "red darken-4"
+      ERROR_COLOR
     else
-      "green darken-1"
+      SUCCESS_COLOR
     end
   end
 end
