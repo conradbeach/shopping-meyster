afe374f8db254648034ddf1a9b6f7daea691ee38
Conrad Beach <conradbeach@gmail.com>
2017-02-01 08:42:26 -0700

    Actually athenticate a user on login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
 
-    if user
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Glad to have you back #{user.username}!"
       redirect_to lists_path
