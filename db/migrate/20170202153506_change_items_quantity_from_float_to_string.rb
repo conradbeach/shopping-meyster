ed9e1fbf9e863f52c91c1369aa35b1a21c422148
Conrad Beach <conradbeach@gmail.com>
2017-02-02 09:18:17 -0700

    Change item quantity from a float to a string.

---
+++ db/migrate/20170202153506_change_items_quantity_from_float_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeItemsQuantityFromFloatToString < ActiveRecord::Migration
+  def change
+    change_column :items, :quantity, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202152152) do
+ActiveRecord::Schema.define(version: 20170202153506) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "price"
-    t.float    "quantity"
+    t.string   "quantity"
     t.boolean  "completed"
     t.integer  "list_id"
   end
